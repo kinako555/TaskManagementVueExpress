2ece015dcb1d89dd36801de65ff160c703c80a99
kinako555 <dodohunter_3658@yahoo.co.jp>
2022-07-09 23:34:12 +0900

    first commit

---
+++ src/views/AfterSigninView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="afterSignin">
+    <h1>Success!!</h1>
+    <SignoutButton/>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+import SignoutButton from '@/components/auth/SignoutButton.vue'
+</script>
