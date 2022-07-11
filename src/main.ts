7db18eb5e86468a8d16f352c831d604e821f45cb
kinako555 <dodohunter_3658@yahoo.co.jp>
2022-07-11 23:01:52 +0900

    クライアントアプリ管理場所変更

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import { initializeFirebase } from './firebase'
-
-initializeFirebase()
-
-createApp(App).use(router).mount('#app')
